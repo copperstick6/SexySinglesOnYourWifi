5225ddb02250fab86bb288a77668a5b449098f83
Claire Dubiel <cdubiel@utexas.edu>
2018-02-24 15:28:36 -0600

    changes to getMatch function

--- backend/app.py
+++ backend/app.py
@@ -12,14 +12,19 @@ def homeRoute():
 	if name == None or SSID == None:
 		return ("Error with request, one or more fields are empty")
 
-	if len(allUsers) == 1:
-		return("There are no matches in your area.")
-	
-	i = 0
-	user = allUsers.keys()[i]
-	while user != SSID:
-		user = allUsers.keys()[i]
-	return("Your match is %s" % str(user))
+	if len(allUsers) == 0: # for debugging
+		return("Error, length should always be >= 1")
+	for ssid_ in allUsers:
+		if ssid_ == SSID:
+			# You are the only one on the wifi network
+			if len(allUsers[ssid_] == 1):
+				return("There are no matches in your area.")
+			i = 0
+			while allUsers[ssid_][i]["name"] == name:
+				i += 1
+			return("Your match it %s" % allUsers[ssid_][i]["name"])
+
+
 
 
 @app.route("/add_user", methods=['POST'])
