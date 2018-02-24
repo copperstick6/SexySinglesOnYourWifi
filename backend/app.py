24302bce6b4731673ae28a2fd6becd817267d89f
copperstick6 <copperstick6@gmail.com>
2018-02-24 14:35:18 -0600

    added current code w/ some map stuff started

--- backend/app.py
+++ backend/app.py
@@ -7,14 +7,23 @@ allUsers = {}
 
 @app.route("/getMatch", methods=['GET'])
 def homeRoute():
-	print(request.args.get("name"))
+	print(request.args.get("SSID"))
+
 	return "Hello World"
 
 
 @app.route("/add_user", methods=['POST'])
 def add_user():
 	name = request.form.get("name")
-	print(name)
+	sex = request.form.get("sex")
+	location = request.form.get("location")
+	preference = request.form.get("preference")
+	SSID = request.form.get("SSID")
+	if name == None  or sex == None or location == None or preference == None or SSID == None:
+		return ("Error with request, one field is empty")
+
+	if SSID in allUsers:
+		allUsers["SSID"]
 	return "Hello World #2"
 
 if __name__ == '__main__':
