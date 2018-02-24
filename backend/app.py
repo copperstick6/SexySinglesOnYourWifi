9eb2668a1e707b6c88423251df33320a728251d7
copperstick6 <copperstick6@gmail.com>
2018-02-24 14:21:42 -0600

    added base app routes

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
 import os
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, request
 
 app = Flask(__name__)
 
-@app.route("/")
+allUsers = {}
+
+@app.route("/getMatch", methods=['GET'])
 def homeRoute():
+	print(request.args.get("name"))
 	return "Hello World"
 
+
+@app.route("/add_user", methods=['POST'])
+def add_user():
+	name = request.form.get("name")
+	print(name)
+	return "Hello World #2"
 
 if __name__ == '__main__':
 	app.run(use_reloader=True, port=5000, threaded=True)
