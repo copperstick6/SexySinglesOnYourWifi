90ee446b5cfdda610ff9397350d530db977a3797
Tintinator <tinsila1@gmail.com>
2018-02-24 16:00:17 -0600

    jesus christ it's jason bourne!

--- android/app/src/main/java/com/example/ty/sexywifi/MainActivity.java
+++ android/app/src/main/java/com/example/ty/sexywifi/MainActivity.java
@@ -1,10 +1,24 @@
 package com.example.ty.sexywifi;
 
+import static java.lang.System.*;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     LocationManager locationManager;
+
+    private static final String[] INITIAL_PERMS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.READ_CONTACTS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,23 +31,37 @@ public class MainActivity extends AppCompatActivity {
         LocationListener locationListener = new LocationListener() {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
-                makeUseOfNewLocation(location);
+                makeUseOfNewLocation("Tin", 'M', location, 'F');
             }
-            public void onStatusChanged(String provider, int status, Bundle extras) {}
 
-            public void onProviderEnabled(String provider) {}
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
 
-            public void onProviderDisabled(String provider) {}
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
         };
-
         // Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+
+
     }
 
     //post location to backend
-    public void makeUseOfNewLocation(){
-
-
+    public void makeUseOfNewLocation(String name, char sex, Location location, char preference){
+        out.println(location.getLatitude() + ", " + location.getLongitude());
     }
 
 
