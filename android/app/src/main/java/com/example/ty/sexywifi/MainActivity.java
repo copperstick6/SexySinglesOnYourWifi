528d97f40270348b389613da54bc7bb0d87b1f57
Tintinator <tinsila1@gmail.com>
2018-02-24 14:56:03 -0600

    permissions boi

--- android/app/src/main/java/com/example/ty/sexywifi/MainActivity.java
+++ android/app/src/main/java/com/example/ty/sexywifi/MainActivity.java
@@ -4,10 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+            public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+            public void onProviderEnabled(String provider) {}
+
+            public void onProviderDisabled(String provider) {}
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
     }
+
+    //post location to backend
+    public void makeUseOfNewLocation(){
+
+
+    }
+
+
 }
