a47dce642509fe69dd57ae9ea72bb35a760a1fbb
Tintinator <tinsila1@gmail.com>
2018-02-25 01:30:27 -0600

    go the distance

--- android/app/src/main/java/com/example/ty/sexywifi/MatchActivity.java
+++ android/app/src/main/java/com/example/ty/sexywifi/MatchActivity.java
@@ -10,6 +10,7 @@ import android.location.LocationManager;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -31,6 +32,9 @@ public class MatchActivity extends AppCompatActivity {
     String ssid;
 
     String mMatch = "";
+
+    boolean isStopped = false;
+
     private static final String[] INITIAL_PERMS = {
             Manifest.permission.ACCESS_FINE_LOCATION,
             Manifest.permission.READ_CONTACTS
@@ -40,18 +44,16 @@ public class MatchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_match);
         getSupportActionBar().hide();
-
         //add RelativeLayout
         mRelativeLayout = findViewById(R.id.activity_main_relative_layout);
 
         //add LayoutParams
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        initProfile();
 
         MatchView match = new MatchView(this);
         match.setLayoutParams(params);
         mRelativeLayout.addView(match);
-
-        mName = "XanTheMan"; mSex = "M"; mPref = "M";
 
         WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService (Context.WIFI_SERVICE);
         WifiInfo info = wifiManager.getConnectionInfo ();
@@ -64,7 +66,7 @@ public class MatchActivity extends AppCompatActivity {
             public void onLocationChanged(Location location) {
                 // Called when a new location is found by the network location provider.
                 loc = location;
-                makeUseOfNewLocation(mName, mSex, location, mPref);
+                makeUseOfNewLocation(mName, location);
             }
 
             public void onStatusChanged(String provider, int status, Bundle extras) {
@@ -87,7 +89,7 @@ public class MatchActivity extends AppCompatActivity {
             // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
         apicaller = new CallAPI(this);
         if(loc == null)
@@ -102,9 +104,15 @@ public class MatchActivity extends AppCompatActivity {
     public void initProfile() {
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         mName = sp.getString("firstname", "NA") + " " + sp.getString("lastname", "NA");
+        if (sp.getString("sex","NA").equals("Male"))
+            mSex = "M";
+        else
+            mSex = "F";
 
-        //mSex;
-        //mPref;
+        if (sp.getString("sex_preference", "NA").equals("Men"))
+            mPref = "M";
+        else
+            mPref = "F";
     }
 
     public void setSexUpdated(boolean state) {
@@ -135,16 +143,33 @@ public class MatchActivity extends AppCompatActivity {
 
     public void setMatchName(String name) {
         if (mMatch.equals("")) {
-            System.out.println( " HEY PAPPY UR LVOER IS : " + name);
             mMatch = name;
 
             getPosition();
         }
     }
 
+    double mLatitude = 0.0;
+    double mLongitude = 0.0;
+    double matchLatitude = 0.0;
+    double matchLongitude = 0.0;
     public void setPosition(String latitude, String longitude) {
-        System.out.println("setPosition: res"  + latitude + " , " + longitude);
-        getPosition();
+        matchLatitude = Double.parseDouble(latitude);
+        matchLongitude = Double.parseDouble(longitude);
+
+        float[] results = new float[2];
+        Location.distanceBetween(mLatitude, mLongitude, matchLatitude, matchLongitude, results);
+        System.out.println("Distance: " + results[0]);
+
+        if (!isStopped) {
+            final Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    getPosition();
+                }
+            }, 3000);
+        }
     }
 
     // Get your matches position
@@ -155,13 +180,13 @@ public class MatchActivity extends AppCompatActivity {
     }
 
     //post location to backend
-    public void makeUseOfNewLocation(String name, String sex, Location location, String preference) {
+    public void makeUseOfNewLocation(String name, Location location) {
         // Update location
         Log.d(TAG, "makeUseOfNewLocation()");
         apicaller = new CallAPI(this);
         apicaller.execute("update_position", name, location.getLatitude()+"", location.getLongitude()+"");
-        // Update SSID if changed, not gonna be relevant lol
-//        apicaller = new CallAPI();
-//        apicaller.execute("update_ssid", mName, ssid);
+
+        mLatitude = location.getLatitude();
+        mLongitude = location.getLongitude();
     }
 }
